d59b5845216fd83ad281b063dae675c8fdf0ed67
lawleitekken <75972731+lawleitekken@users.noreply.github.com>
2021-09-26 23:43:43 +0530

    Update gofiles.py

--- plugins/gofiles.py
+++ plugins/gofiles.py
@@ -65,7 +65,7 @@ async def query_mgs(client: Bot, message: Message):
                                 chat_id=message.from_user.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
-                                caption=messages.caption+Presets.CAPTION_TEXT_DOC.format(media_name, media_format, file_size)
+                                caption=message.caption+Presets.CAPTION_TEXT_DOC.format(media_name, media_format, file_size)
                             )
                         except FloodWait as e:
                             time.sleep(e.x)
@@ -102,7 +102,7 @@ async def query_mgs(client: Bot, message: Message):
                                 chat_id=message.from_user.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
-                                caption=messages.caption+Presets.CAPTION_TEXT_VID.format(media_name, vid_file_names, file_size)
+                                caption=message.caption+Presets.CAPTION_TEXT_VID.format(media_name, file_size)
                             )
                         except FloodWait as e:
                             time.sleep(e.x)
