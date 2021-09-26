b019458481ee00f0bdacf921c759b8cb99ccd0fc
lawleitekken <75972731+lawleitekken@users.noreply.github.com>
2021-09-27 01:17:40 +0530

    Update gofiles.py

--- plugins/gofiles.py
+++ plugins/gofiles.py
@@ -40,7 +40,6 @@ async def query_mgs(client: Bot, message: Message):
                         try:
                             await client.send_chat_action(
                                 chat_id=message.from_user.id,
-                                caption=message.caption,
                                 action="upload_document"
                             )
                         except Exception:
@@ -66,7 +65,7 @@ async def query_mgs(client: Bot, message: Message):
                                 chat_id=message.from_user.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
-                                caption=message.caption+Presets.CAPTION_TEXT_DOC.format(media_name, media_format, file_size)
+                                caption=message.caption
                             )
                         except FloodWait as e:
                             time.sleep(e.x)
@@ -104,7 +103,7 @@ async def query_mgs(client: Bot, message: Message):
                                 chat_id=message.from_user.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
-                                caption=message.caption+Presets.CAPTION_TEXT_VID.format(media_name, file_size)
+                                caption=messages.caption
                             )
                         except FloodWait as e:
                             time.sleep(e.x)
