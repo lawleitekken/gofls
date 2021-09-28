ee9cf786c98dcc78958740a54e3f9d9b97bf94cf
lawleitekken <75972731+lawleitekken@users.noreply.github.com>
2021-09-29 04:19:02 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -13,10 +13,10 @@ class Presets(object):
 
     BOT_PM_TEXT = "<b>Sorry.. 😢</b>\n\n<code>Bot won't work in PM, Ask in ma Group. I'll reply the file in PM if " \
                   "available in our DB !</code>"
-    PM_ERROR = "<b>Unable to send medias</b> ⛔️\n<code>Click the below button\nAsk here for movies later!</code>"
-    MEDIA_SEND_TEXT = "<code>Media dispatched as PM 🥳</code>"
+    PM_ERROR = "<b>Unable to send movie</b> ⛔️\n<code>Click the below button\nAsk here for movies later!</code>"
+    MEDIA_SEND_TEXT = "<code> 𝙃𝙚𝙧𝙚 𝙔𝙤𝙪𝙧 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙈𝙤𝙫𝙞𝙚🥳</code>"
     NO_MEDIA = "Requested movie: <b>{}</b>\n\n<b>Not available " \
                "Right Now</b>\n<code>Possible Causes : 🤔\n\n⭕️ Not " \
                "released yet</code>\n⭕️ <a href='https://www.google.com/search?q={}'> Spelled incorrectly</a>\n" \
                "<code>⭕️ Unwanted texts in Msgs\n⭕ Asking theatre prints\n⭕ Not in ma Database</code>"
-    BLOCK_LIST = []
+    BLOCK_LIST = ['http://', 'https://', '@', '#', 'bit.ly', 't.me', '/']
