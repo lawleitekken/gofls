824236aac97a7a16f2ad3da48e5063b677c2f9e7
lawleitekken <75972731+lawleitekken@users.noreply.github.com>
2021-10-12 02:11:23 +0530

    Update gofiles.py

--- plugins/gofiles.py
+++ plugins/gofiles.py
@@ -124,8 +124,6 @@ async def query_mgs(client: Bot, message: Message):
                 pass
             return
         if user_message.keys():
-        else:
-            updated_query = query_message.replace(" ", "+")
             try:
                 await client.send_message(
                     chat_id=message.chat.id,
