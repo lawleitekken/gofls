d7d6a03924e8d32e32ea4d925ea18ad20e21b895
lawleitekken <75972731+lawleitekken@users.noreply.github.com>
2021-10-19 02:51:56 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -15,11 +15,11 @@ class Presets(object):
 
     BOT_PM_TEXT = "<b>Sorry.. 😢</b>\n\n<code>Bot won't work in PM, Ask in ma Group. I'll reply the file in PM if " \
                   "available in our DB !</code>"
-    PM_ERROR = "<b>Unable to send movie</b> ⛔️\n<code>Click the below button\nTo Inform this to admin for update. #Unable </code>"
+    PM_ERROR = "<b>Unable to send movie</b> ⛔️\n<code>𝐂𝐥𝐢𝐜𝐤 𝐓𝐡𝐞 𝐁𝐞𝐥𝐨𝐰 𝐁𝐮𝐭𝐭𝐨𝐧\n𝐓𝐨 𝐈𝐧𝐟𝐨𝐫𝐦 𝐭𝐡𝐢𝐬 𝐭𝐨 𝐚𝐝𝐦𝐢𝐧 𝐥𝐢𝐤𝐞 𝐭𝐡𝐢𝐬 𝐌𝐨𝐯𝐢𝐞𝐍𝐚𝐦𝐞 𝐰𝐢𝐭𝐡 #𝐮𝐧𝐚𝐛𝐥𝐞 𝐅𝐨𝐫 𝐔𝐩𝐝𝐚𝐭𝐞 𝐄𝐱 : #𝐮𝐧𝐚𝐛𝐥𝐞 𝐒𝐩𝐢𝐝𝐞𝐫𝐌𝐚𝐧 </code>"
     MEDIA_SEND_TEXT = "𝐘𝐨𝐮𝐫 𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐌𝐨𝐯𝐢𝐞: <b>{}</b>\n\n"
     NO_MEDIA = "𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐌𝐨𝐯𝐢𝐞: <b>{}</b>\n\n𝐒𝐨𝐫𝐫𝐲, 𝐍𝐨 𝐌𝐨𝐯𝐢𝐞 𝐑𝐞𝐥𝐚𝐭𝐞𝐝 𝐭𝐨 𝐭𝐡𝐞 𝐆𝐢𝐯𝐞𝐧 𝐖𝐨𝐫𝐝 🥺 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐨𝐥𝐥𝐨𝐰 𝐁𝐞𝐥𝐨𝐰 #𝐏𝐨𝐢𝐧𝐭𝐬 \n\n" \
                "⭕️ 𝐏𝐥𝐞𝐚𝐬𝐞 𝐂𝐡𝐞𝐜𝐤 <a href='https://www.google.com/search?q={}'> 𝐒𝐩𝐞𝐥𝐥𝐢𝐧𝐠 𝐢𝐧 𝐆𝐨𝐨𝐠𝐥𝐞 </a>\n" \
                "⭕️ 🔎 𝐅𝐨𝐫𝐦𝐚𝐭 : 𝐒𝐩𝐢𝐝𝐞𝐫𝐌𝐚𝐧 𝟐𝟎𝟐𝟏 𝐓𝐞𝐥𝐮𝐠𝐮 \n" \
                "⭕️ 𝐒𝐭𝐢𝐥𝐥 𝐍𝐨 𝐑𝐞𝐬𝐮𝐥𝐭𝐬 𝐏𝐥𝐳 𝐒𝐞𝐧𝐝 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐈𝐧\n" \
-               "⭕️ <b>Request Group: </b><a href='https://t.me/justwatch_movies1'> 𝐆𝐫𝐨𝐮𝐩</a>"
+               "⭕️ <b>Request Group: </b><a href='https://t.me/justwatch_movies1'> 𝐆𝐫𝐨𝐮𝐩</a> 𝐄𝐱:#𝐫𝐞𝐪 𝐒𝐩𝐢𝐝𝐞𝐫𝐌𝐚𝐧"
     BLOCK_LIST = ['http://', 'https://', '@', '#', 'bit.ly', 't.me', '/']
