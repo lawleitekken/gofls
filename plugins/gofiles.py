5a026fd80a5da6e59ccc33585c858557ab6a9cc1
lawleitekken <75972731+lawleitekken@users.noreply.github.com>
2021-11-18 14:35:29 +0530

    Update gofiles.py

--- plugins/gofiles.py
+++ plugins/gofiles.py
@@ -70,15 +70,54 @@ async def query_mgs(client: Bot, message: Message):
                         except FloodWait as e:
                             time.sleep(e.x)
                         user_message[id] = message.message_id
-                # Looking for video type in messages
-                async for messages in client.USER.search_messages(channel, query_message, filter="document", limit=10):
-                    vid_file_names = message.caption
-                    #file_size = get_size(messages.document.file_size)
-                    if re.compile(rf'{vid_file_names}', re.IGNORECASE):
+                # Looking for Document type in messages
+                async for messages in client.USER.search_messages(channel, query_message, filter="document", limit=50):
+                    doc_file_names = messages.document.file_name
+                    file_size = get_size(messages.document.file_size)
+                    if re.compile(rf'{doc_file_names}', re.IGNORECASE):
                         try:
                             await client.send_chat_action(
                                 chat_id=message.from_user.id,
                                 action="upload_document"
+                            )
+                        except Exception:
+                            query_bytes = query_message.encode("ascii")
+                            base64_bytes = b64encode(query_bytes)
+                            secret_query = base64_bytes.decode("ascii")
+                            await client.send_message(
+                                chat_id=message.chat.id,
+                                text=Presets.ASK_PM_TEXT,
+                                reply_to_message_id=message.message_id,
+                                reply_markup=InlineKeyboardMarkup(
+                                    [
+                                        [InlineKeyboardButton(
+                                            "👉 CLICK HERE 👈", url="t.me/{}?start={}".format(info.username, secret_query))
+                                         ]
+                                    ])
+                            )
+                            return
+                        media_name = messages.document.file_name.rsplit('.', 1)[0]
+                        media_format = messages.document.file_name.split('.')[-1]
+                        try:
+                            await client.copy_message(
+                                chat_id=message.from_user.id,
+                                from_chat_id=messages.chat.id,
+                                message_id=messages.message_id,
+                                caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_DOC.format(media_name,
+                                                                                            media_format, file_size)
+                            )
+                        except FloodWait as e:
+                            time.sleep(e.x)
+                        user_message[id] = message.message_id
+                # Looking for video type in messages
+                async for messages in client.USER.search_messages(channel, query_message, filter="video", limit=50):
+                    vid_file_names = messages.caption
+                    file_size = get_size(messages.video.file_size)
+                    if re.compile(rf'{vid_file_names}', re.IGNORECASE):
+                        try:
+                            await client.send_chat_action(
+                                chat_id=message.from_user.id,
+                                action="upload_video"
                             )
                         except Exception:
                             query_bytes = query_message.encode("ascii")
@@ -102,16 +141,32 @@ async def query_mgs(client: Bot, message: Message):
                                 chat_id=message.from_user.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
-                                caption=message.caption
+                                caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_VID.format(media_name, file_size)
                             )
                         except FloodWait as e:
                             time.sleep(e.x)
                         user_message[id] = message.message_id
+        except Exception:
+            try:
+                await client.send_message(
+                    chat_id=message.chat.id,
+                    text=Presets.PM_ERROR,
+                    reply_to_message_id=message.message_id,
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [InlineKeyboardButton(
+                                "👉 START BOT 👈", url="t.me/{}".format(info.username))
+                             ]
+                        ])
+                )
+            except Exception:
+                pass
+            return
         if user_message.keys():
             try:
                 await client.send_message(
                     chat_id=message.chat.id,
-                    text=Presets.MEDIA_SEND_TEXT.format(query_message),
+                    text=Presets.MEDIA_SEND_TEXT,
                     reply_to_message_id=user_message[id],
                     reply_markup=InlineKeyboardMarkup(
                         [
@@ -121,8 +176,19 @@ async def query_mgs(client: Bot, message: Message):
                         ])
                 )
                 user_message.clear()
-                #await msg.delete()
-                #await message.delete()
             except Exception:
                 pass
+        else:
+            updated_query = query_message.replace(" ", "+")
+            try:
+                await client.send_message(
+                    chat_id=message.chat.id,
+                    text=Presets.NO_MEDIA.format(query_message, updated_query),
+                    reply_to_message_id=message.message_id,
+                    parse_mode='html',
+                    disable_web_page_preview=True
+                )
+            except Exception:
+                pass   
+                            
         
