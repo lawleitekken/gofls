cbffd9e0992a94524815bbd763bf228d6d314043
lawleitekken <75972731+lawleitekken@users.noreply.github.com>
2021-11-18 14:11:05 +0530

    Update pm_query.py

--- plugins/pm_query.py
+++ plugins/pm_query.py
@@ -75,36 +75,15 @@ async def bot_pm(client: Bot, message: Message):
                             )
                         except FloodWait as e:
                             time.sleep(e.x)
-                # Looking for Document type in messages
-                async for messages in client.USER.search_messages(channel, secret_query, filter="document", limit=50):
-                    doc_file_names = messages.document.file_name
-                    file_size = get_size(messages.document.file_size)
-                    if re.compile(rf'{doc_file_names}', re.IGNORECASE):
-                        media_name = messages.document.file_name.rsplit('.', 1)[0]
-                        media_format = messages.document.file_name.split('.')[-1]
-                        await client.send_chat_action(
-                            chat_id=message.from_user.id,
-                            action="upload_document"
-                        )
-                        try:
-                            await client.copy_message(
-                                chat_id=message.chat.id,
-                                from_chat_id=messages.chat.id,
-                                message_id=messages.message_id,
-                                caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_DOC.format(media_name,
-                                                                                            media_format, file_size)
-                            )
-                        except FloodWait as e:
-                            time.sleep(e.x)
                 # Looking for video type in messages
-                async for messages in client.USER.search_messages(channel, secret_query, filter="video", limit=50):
+                async for messages in client.USER.search_messages(channel, secret_query, filter="document", limit=10):
                     vid_file_names = messages.caption
-                    file_size = get_size(messages.video.file_size)
+                    file_size = get_size(messages.document.file_size)
                     if re.compile(rf'{vid_file_names}', re.IGNORECASE):
                         media_name = secret_query.upper()
                         await client.send_chat_action(
                             chat_id=message.from_user.id,
-                            action="upload_video"
+                            action="upload_document"
                         )
                         try:
                             await client.copy_message(
