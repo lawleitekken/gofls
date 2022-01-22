556963ebcc6f296f1f0bbe6bce3924541d009a53
lawleitekken <75972731+lawleitekken@users.noreply.github.com>
2022-01-23 03:24:03 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -9,17 +9,17 @@ class Presets(object):
                   
     CLEAN_CHAT_MSG = "⚠️ <b>Deleting all messages..</b>"
     MSG_FOR_PIN = "<b>𝐅𝐨𝐫 𝐆𝐞𝐭𝐭𝐢𝐧𝐠 𝐌𝐨𝐯𝐢𝐞 𝐋𝐢𝐧𝐤𝐬 𝐟𝐫𝐨𝐦 𝐡𝐞𝐫𝐞..</b>\n\n🔛 <code> 𝐏𝐥𝐞𝐚𝐬𝐞 𝐬𝐭𝐚𝐫𝐭 </code> @{} <code>𝐢𝐧 𝐏𝐌\n\n" \
-                  "𝐏𝐥𝐞𝐚𝐬𝐞 𝐒𝐞𝐚𝐫𝐜𝐡 𝐌𝐨𝐯𝐢𝐞𝐬 𝐖𝐢𝐭𝐡 𝐌𝐨𝐯𝐢𝐞 𝐍𝐚𝐦𝐞 𝐎𝐧𝐥𝐲 𝐃𝐨𝐧'𝐭 𝐓𝐲𝐩𝐞 𝐀𝐧𝐲 𝐎𝐭𝐡𝐞𝐫 𝐖𝐨𝐫𝐝𝐬.\n"\
+                  "𝐏𝐥𝐞𝐚𝐬𝐞 𝐒𝐞𝐧𝐝 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐖𝐢𝐭𝐡 𝐌𝐨𝐯𝐢𝐞 𝐍𝐚𝐦𝐞 𝐎𝐧𝐥𝐲 𝐃𝐨𝐧'𝐭 𝐓𝐲𝐩𝐞 𝐀𝐧𝐲 𝐎𝐭𝐡𝐞𝐫 𝐖𝐨𝐫𝐝𝐬.\n"\
                   "𝐒𝐩𝐢𝐝𝐞𝐫𝐌𝐚𝐧 ✅\n"\
                   "𝐒𝐩𝐢𝐝𝐞𝐫𝐌𝐚𝐧 𝐓𝐞𝐥𝐮𝐠𝐮 𝐃𝐮𝐛𝐛𝐞𝐝 ❌ \n\n🔊 𝐈'𝐥𝐥 𝐫𝐞𝐩𝐥𝐲 𝐭𝐡𝐞 𝐌𝐨𝐯𝐢𝐞 𝐋𝐢𝐧𝐤𝐬 𝐢𝐧 𝐏𝐌 𝐢𝐟 𝐚𝐯𝐚𝐢𝐥𝐚𝐛𝐥𝐞 𝐢𝐧 𝐨𝐮𝐫 𝐜𝐡𝐚𝐧𝐧𝐞𝐥 !</code>"
 
     BOT_PM_TEXT = "<b>Sorry.. 😢</b>\n\n<code>Bot won't work in PM, Ask in ma Group. I'll reply the file in PM if " \
                   "available in our DB !</code>"
-    PM_ERROR = "<b>Unable to send movie</b> ⛔️\n<code>𝐂𝐥𝐢𝐜𝐤 𝐓𝐡𝐞 𝐁𝐞𝐥𝐨𝐰 𝐁𝐮𝐭𝐭𝐨𝐧\n𝐓𝐨 𝐈𝐧𝐟𝐨𝐫𝐦 𝐭𝐡𝐢𝐬 𝐭𝐨 𝐚𝐝𝐦𝐢𝐧 𝐥𝐢𝐤𝐞 𝐭𝐡𝐢𝐬 𝐌𝐨𝐯𝐢𝐞𝐍𝐚𝐦𝐞 𝐰𝐢𝐭𝐡 #𝐮𝐧𝐚𝐛𝐥𝐞 𝐅𝐨𝐫 𝐔𝐩𝐝𝐚𝐭𝐞 𝐄𝐱 : #𝐮𝐧𝐚𝐛𝐥𝐞 𝐒𝐩𝐢𝐝𝐞𝐫𝐌𝐚𝐧 </code>"
+    PM_ERROR = "<b>Unable to send movie</b>"
     MEDIA_SEND_TEXT = "𝐘𝐨𝐮𝐫 𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐌𝐨𝐯𝐢𝐞: <b>{}</b>\n\n"
     NO_MEDIA = "𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐌𝐨𝐯𝐢𝐞: <b>{}</b>\n\n𝐒𝐨𝐫𝐫𝐲, 𝐍𝐨 𝐌𝐨𝐯𝐢𝐞 𝐑𝐞𝐥𝐚𝐭𝐞𝐝 𝐭𝐨 𝐭𝐡𝐞 𝐆𝐢𝐯𝐞𝐧 𝐖𝐨𝐫𝐝 🥺 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐨𝐥𝐥𝐨𝐰 𝐁𝐞𝐥𝐨𝐰 #𝐏𝐨𝐢𝐧𝐭𝐬 \n\n" \
                "⭕️ 𝐏𝐥𝐞𝐚𝐬𝐞 𝐂𝐡𝐞𝐜𝐤 <a href='https://www.google.com/search?q={}'> 𝐒𝐩𝐞𝐥𝐥𝐢𝐧𝐠 𝐢𝐧 𝐆𝐨𝐨𝐠𝐥𝐞 </a>\n" \
                "⭕️ 🔎 𝐅𝐨𝐫𝐦𝐚𝐭 : 𝐒𝐩𝐢𝐝𝐞𝐫𝐌𝐚𝐧 𝟐𝟎𝟐𝟏 𝐓𝐞𝐥𝐮𝐠𝐮 \n" \
                "⭕️ 𝐒𝐭𝐢𝐥𝐥 𝐍𝐨 𝐑𝐞𝐬𝐮𝐥𝐭𝐬 𝐏𝐥𝐳 𝐒𝐞𝐧𝐝 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐈𝐧\n" \
-               "⭕️ <a href='https://t.me/justwatch_movies1'>𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐆𝐫𝐨𝐮𝐩 </a> 𝐄𝐱:#𝐫𝐞𝐪 𝐒𝐩𝐢𝐝𝐞𝐫𝐌𝐚𝐧"
+               "⭕️ <a href='https://t.me/'>𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐆𝐫𝐨𝐮𝐩 </a>"
     BLOCK_LIST = ['http://', 'https://', '@', '#', 'bit.ly', 't.me', '/']
